ebe9f14e7d262adc20e811df00ed28d2c46c4328
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 16:20:19 +0200

    exo6

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 6</title>
+</head>
+<body>
+  <p>
+    <?php
+    $name = 'Cédric';
+    echo 'Bonjour ' . $name . ', comment vas-tu?';
+    ?>
+  </p>
+</body>
+</html>
